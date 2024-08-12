9b0f914d50df84084e2084f4c2d4727c8b341129
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2024-08-12 11:45:38 +0200

    Add config.py

---
+++ hoa_exec/config.py
@@ -0,0 +1,61 @@
+from abc import ABC
+from pathlib import Path
+
+import tomlkit
+from tomlkit.exceptions import TOMLKitError
+from tomlkit.items import String
+from tomlkit import TOMLDocument
+
+from .drivers import CompositeDriver, DRIVERS, UserDriver
+
+
+class Configuration(ABC):
+    def get_driver(self):
+        return self.driver
+
+    @staticmethod
+    def factory(fname: Path, aps: list):
+        if fname.suffix != ".toml":
+            raise NotImplementedError(f"Unsupported config format {fname.suffix}.")  # noqa: E501
+        with open("config.toml") as conf_file:
+            try:
+                conf = tomlkit.load(conf_file)
+            except TOMLKitError as err:
+                raise IOError(err) from None
+        assert "hoa-exec" in conf, "configuration file missing mandatory element '[hoa-exec]'"  # noqa: E501
+        assert "version" in conf['hoa-exec'], "configuration file missing mandatory element '[hoa-exec]version'"  # noqa: E501
+        conf_version = conf["hoa-exec"]["version"]
+        assert conf_version == 1, f"unsupported configuration version: {conf_version}"  # noqa: E501
+        if conf_version == 1:
+            return TomlConfigV1(fname, conf, aps)
+        raise Exception("???")
+
+
+class DefaultConfig(Configuration):
+    def __init__(self, aps: list[str]) -> None:
+        self.driver = UserDriver(aps)
+
+
+class TomlConfigV1(Configuration):
+
+    def __init__(self, fname: Path, conf: TOMLDocument, aps: list[str]) -> None:  # noqa: E501
+        d = CompositeDriver()
+        print(DRIVERS)
+        # If no default driver is given, pick user driver
+        default = conf["hoa-exec"].get("default-driver", "user")
+        default_driver = DRIVERS[default]
+        aps_left = set(aps)
+        for key in DRIVERS:
+            if key in conf.get("driver", []):
+                ap_str = [
+                    str(i) if type(i) is String else aps[i]
+                    for i in conf["driver"][key].get("aps",  [])]
+                if ap_str:
+                    d.append(DRIVERS[key](ap_str))
+                    aps_left.difference_update(ap_str)
+        if aps_left:
+            d.append(default_driver(list(aps_left)))
+        self.driver = d
+
+    def get_driver(self):
+        return self.driver
