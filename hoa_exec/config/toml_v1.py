873744fbd856f49766e397c930c19a2165b48780
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2024-08-13 16:42:50 +0200

    Update TOMLv1 config format

--- hoa_exec/config/toml_v1.py
+++ hoa_exec/config/toml_v1.py
@@ -1,19 +1,28 @@
-from typing import Annotated, Optional
+from typing import Annotated, Collection, Optional
 
 from msgspec import Meta, Struct, field
 
 
+def invalid(field: str, valid: Collection[str]):
+    valid = ', '.join(valid)
+    raise ValueError(f"{field} must be one of {valid}") from None
+
+
 class TomlV1(Struct):
+
     class HoaExec(Struct):
         DRIVERS = "flip", "user"
+        LOG_LEVELS = "none", "error", "info", "debug"
         version: Annotated[int, Meta(ge=1, le=1)]
         name: Optional[str] = None
         default_driver: Optional[str] = field(name="default-driver", default="user")  # noqa: E501
+        log_level: Optional[str] = field(name="log-level", default="info")  # noqa: E501
 
         def __post_init__(self):
             if self.default_driver not in self.DRIVERS:
-                raise ValueError(
-                    f"default-driver must be one of {', '.join(self.DRIVERS)}")
+                invalid("default-driver", self.DRIVERS)
+            if self.log_level not in self.LOG_LEVELS:
+                invalid("log-level", self.LOG_LEVELS)
 
     class DriverSection(Struct):
         class RandomDriver(Struct):
@@ -26,5 +35,16 @@ class TomlV1(Struct):
         flip: list[RandomDriver] = field(default_factory=list)
         user: list[UserDriver] = field(default_factory=list)
 
+    class RunnerSection(Struct):
+        NONDET_VALUES = ("first", "random", "user")
+
+        bound: Annotated[int, Meta(gt=0)] = None
+        nondet: Optional[str] = field(default="first")
+
+        def __post_init__(self):
+            if self.nondet not in self.NONDET_VALUES:
+                invalid("nondet", self.NONDET_VALUES)
+
     hoa_exec: HoaExec = field(name="hoa-exec")
-    driver: DriverSection
+    driver: DriverSection = field(default_factory=DriverSection)
+    runner: RunnerSection = field(default_factory=RunnerSection)
