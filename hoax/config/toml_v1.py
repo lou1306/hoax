7aec456f06dc54f041a4eadfb855f86c6f609570
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2026-01-30 15:14:29 +0100

    fix default seed in TOML config

--- hoax/config/toml_v1.py
+++ hoax/config/toml_v1.py
@@ -40,14 +40,12 @@ class TomlV1(Struct):
         version: Annotated[int, Meta(ge=1, le=1)]
         name: Optional[str] = None
         default_driver: Optional[str] = field(name="default-driver", default="user")  # noqa: E501
-        seed: Optional[Annotated[int, Meta(ge=0)]] = field(name="seed", default=None)  # noqa: E501
+        seed: Annotated[int, Meta(ge=0)] = field(name="seed", default=PRG_DEFAULT_SEED)  # noqa: E501
 
         def get_default_driver(self):
             return self.DRIVERS[self.default_driver]
 
         def __post_init__(self):
-            if self.seed is None:
-                self.seed = PRG_DEFAULT_SEED
             if self.default_driver not in self.DRIVERS:
                 invalid("default-driver", self.DRIVERS)
 
